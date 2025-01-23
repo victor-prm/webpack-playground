5478eb17a1ad12035c60625a1475bb836bdca421
victor-prm <victorpabb@gmail.com>
2025-01-23 23:03:26 +0100

    added index.html

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,20 @@
-const path  = require('path');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     mode: 'development',
     entry: {
         main: path.resolve(__dirname, 'src/app.js')
     },
-    output:{
+    output: {
         path: path.resolve(__dirname, 'dist'),
         filename: '[name].[contenthash].js',
         clean: true,
-    }
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Just a Demo',
+            filename: 'index.html',
+        }),
+    ],
 }
